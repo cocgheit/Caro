f4514f9ce6002ce0c9aa970a3f334b8a9a016eba
meodaymo <73814005+meodaymo@users.noreply.github.com>
2020-12-12 00:35:44 +0700

    Giao diện home

--- app/src/main/java/vn/edu/vnu/hus/caro/MainActivity.java
+++ app/src/main/java/vn/edu/vnu/hus/caro/MainActivity.java
@@ -6,20 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import vn.edu.vnu.hus.caro.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn1,btn2;
+    private Button btn_login,btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button) findViewById(R.id.button6);
-        btn2 = (Button) findViewById(R.id.button5);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_register = (Button) findViewById(R.id.btn_register);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, vn.edu.vnu.hus.caro.view.home.class);
@@ -27,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn2.setOnClickListener(new View.OnClickListener()
+        btn_register.setOnClickListener(new View.OnClickListener()
     {
         public void onClick(View v)
         {
