69a253a06790a0abec0ef6507c3a58886084cbcc
meodaymo <73814005+meodaymo@users.noreply.github.com>
2020-12-12 17:40:24 +0700

    commit lại giao diện

--- app/src/main/java/vn/edu/vnu/hus/caro/MainActivity.java
+++ app/src/main/java/vn/edu/vnu/hus/caro/MainActivity.java
@@ -6,19 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import vn.edu.vnu.hus.caro.R;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btn1,btn2, btnTest;
+    private Button btn1,btn2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn1 = (Button) findViewById(R.id.button6);
-        btn2 = (Button) findViewById(R.id.button5);
-        btnTest = this.findViewById(R.id.btnTest);
+        btn1 = (Button) findViewById(R.id.btn_login);
+        btn2 = (Button) findViewById(R.id.btn_register);
+
+        //btnTest = this.findViewById(R.id.btnTest);
+
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         btn2.setOnClickListener(new View.OnClickListener()
-        {
+
+    {
+        
             public void onClick(View v)
             {
                 Intent intent = new Intent(MainActivity.this, vn.edu.vnu.hus.caro.view.register.class);
@@ -40,13 +44,14 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Bắt sự kiện cho nút test.
-        btnTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent testIntent = new Intent(MainActivity.this, vn.edu.vnu.hus.caro.view.Test.class);
-                MainActivity.this.startActivity(testIntent);
-            }
-        });
+//        btnTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent testIntent = new Intent(MainActivity.this, vn.edu.vnu.hus.caro.view.Test.class);
+//                MainActivity.this.startActivity(testIntent);
+//            }
+//        });
 
+
+        }
     }
-}
---
+++ app/src/main/java/vn/edu/vnu/hus/caro/view/drawChessBoard.java
@@ -0,0 +1,15 @@
+package vn.edu.vnu.hus.caro.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import vn.edu.vnu.hus.caro.R;
+
+public class drawChessBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_chess_board);
+    }
+}
